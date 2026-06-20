05893c024897bb9f79b91f68fc688efc07cc55bc
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-20 00:43:26 -0300

    Add ola mundo pelo console

---
+++ secao002/aula006-2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace aula006_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, mundo!! .Net");
+        }
+    }
+}
