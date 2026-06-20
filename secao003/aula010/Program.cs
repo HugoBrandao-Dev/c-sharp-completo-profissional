c6956a40fe21a4ecfc7c31749264f0908bb0d556
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-20 15:29:17 -0300

    Add bloco region

---
+++ secao003/aula010/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace aula010
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Aracnídeos
+            Console.WriteLine("Escorpião");
+            Console.WriteLine("Aranha");
+            Console.WriteLine("Carrapatos");
+            Console.WriteLine("Ácaros");
+            #endregion
+
+            #region Insetos
+            Console.WriteLine("Formiga");
+            Console.WriteLine("Borboleta");
+            Console.WriteLine("Cupim");
+            Console.WriteLine("Vespa");
+            #endregion
+        }
+    }
+}
