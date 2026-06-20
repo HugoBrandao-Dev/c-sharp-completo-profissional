eb44f947a4690ca83357a8af88836352946fcdc3
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-20 14:58:28 -0300

    Add comentarios inline e bloco

---
+++ secao003/aula009/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace aula009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Comentario de uma linha.
+            
+            /*
+             * Bloco de comentarios.
+             */
+
+            // O codigo abaixo mostra o meu nome.
+            // Console.WriteLine("Meu nome é Hugo.");
+
+            Console.WriteLine("Meu nome é Tobias;");
+            Console.WriteLine("Meu nome é Dinorá;");
+            Console.WriteLine("Meu nome é Josias;");
+            Console.WriteLine("Meu nome é Doralice.");
+        }
+    }
+}
