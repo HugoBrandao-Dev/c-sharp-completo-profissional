14fed9937b8c5de41a2e7a580729db149191fa0f
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-21 16:48:09 -0300

    Add tipos reais

---
+++ secao004/aula013/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace aula013
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Reais
+            float meuFloat = 1.5f;
+            double meuDouble = 12.32;
+            decimal meuDecimal = 25.5643m;
+            #endregion
+
+            double valor;
+
+            valor = 102.1324;
+            valor = meuDouble;
+
+            Console.WriteLine(valor);
+            Console.ReadKey();
+        }
+    }
+}
