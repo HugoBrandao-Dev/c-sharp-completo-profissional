9e8d57832a43e8d5c094a207b79d35531e910b00
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-21 20:51:36 -0300

    Add tipo var

---
+++ secao004/aula017/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula017
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Tipo var
+            var produto = "Creme dental";
+            var preco = 7.5;
+            var haEstoque = true;
+
+            haEstoque = false;
+            #endregion
+
+            Console.WriteLine(produto);
+            Console.WriteLine(preco);
+            Console.WriteLine(haEstoque);
+        }
+    }
+}
