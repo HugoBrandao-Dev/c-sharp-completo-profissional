469cbb132ac54df3707dab2f21fe26801d5d3b77
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-21 17:08:03 -0300

    Add tipo literal/caractere

---
+++ secao004/aula014/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula014
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Caracteres
+            char minhaLetra = 'h'; // Literal
+            char letraCodigo = '\u0041';
+            char escape = '\n';
+            char barraInver = '\\';
+            #endregion
+
+            Console.WriteLine(minhaLetra);
+            Console.WriteLine(escape);
+            Console.WriteLine(letraCodigo);
+            Console.WriteLine(barraInver);
+        }
+    }
+}
