6dbb06baef90b068440239820105ef5038169d7f
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-22 13:11:11 -0300

    Add tipo enum

---
+++ secao004/aula020/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula020
+{
+    internal class Program
+    {
+        enum Notas
+        { // O valor padrao e o indice, caso contrario, e valor, valor+1, valor+2...
+            min, med = 21, max
+        }
+        static void Main(string[] args)
+        {
+            Notas notasAluno = Notas.med;
+        }
+    }
+}
