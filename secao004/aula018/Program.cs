0e7af081f7fe7a9e1fa7a2e8d52f7f9b9a709d23
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-21 21:19:21 -0300

    Add tipo object

---
+++ secao004/aula018/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula018
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object meuObj = "Josias";
+            Console.WriteLine(meuObj);
+
+            /*
+             * Mesmo sendo declarada com valor do tipo string,
+             * pode ser mudado por outro tipo.
+             */
+            meuObj = 200;
+            Console.WriteLine(meuObj);
+        }
+    }
+}
