523798065ab011b064e967130b384ed66104b0b6
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-21 21:29:46 -0300

    Add constante

---
+++ secao004/aula019/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.1416;
+            const string nome = "Dinorá";
+
+            Console.WriteLine(pi);
+            Console.WriteLine(nome);
+        }
+    }
+}
