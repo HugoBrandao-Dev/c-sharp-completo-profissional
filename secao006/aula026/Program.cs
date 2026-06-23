96e8c22ed1b9bcd3e163818db44c74c3eedec877
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-23 14:24:41 -0300

    Add metodo parse

---
+++ secao006/aula026/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula026
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte meuByte = byte.Parse("123");
+            Console.WriteLine(meuByte);
+
+            // Dara erro, pq tem letra no valor
+            // ushort meuUshort = ushort.Parse("918a");
+
+            long meuLong = long.Parse("1000000");
+            Console.WriteLine(meuLong);
+
+            float meuFloat = float.Parse("123,321");
+            Console.WriteLine(meuFloat);
+        }
+    }
+}
---
+++ secao006/aula026/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("aula026")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("aula026")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("5c0c1012-1bb5-4857-aefc-3f2aa916dfb1")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
