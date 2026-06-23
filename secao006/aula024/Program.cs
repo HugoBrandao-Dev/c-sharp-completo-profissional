70c02160a0ff6f2261591a7aba88e2302071406c
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-23 11:26:11 -0300

    Add conversao implicita

---
+++ secao006/aula024/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * O ushort comporta o valor de byte.
+             */
+            byte meuByte = 100; // 8 bits, de 0 a 255
+            ushort meuUShort; // 16 bits, 0 a 65.535
+            meuUShort = meuByte;
+            Console.WriteLine(meuUShort);
+
+            /*
+             * O double comporta o valor de float.
+             */
+            float meuFloat = 1250.45f;
+            double meuDouble = meuFloat;
+            Console.WriteLine(meuDouble);
+
+            /*
+             * int comporta o VALOR (ASCII) de letra.
+             */
+            char letra = 'h';
+            int numChar = letra;
+            Console.WriteLine(numChar);
+        }
+    }
+}
