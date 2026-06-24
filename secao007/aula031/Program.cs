9974aeeece651d473ef8ffd93794d45e85d799c9
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-24 17:41:03 -0300

    Add operador concatenacao

---
+++ secao007/aula031/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula031
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "Josias";
+            string sobreNome = "Cruz";
+            string nomeCompleto = nome + " " + sobreNome;
+
+            Console.WriteLine(nomeCompleto);
+        }
+    }
+}
