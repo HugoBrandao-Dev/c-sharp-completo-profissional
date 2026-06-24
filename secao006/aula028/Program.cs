a14ee029ee07bc58da809e46c5ff800275b59d8e
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-24 12:23:28 -0300

    Add operadores aritmeticos

---
+++ secao006/aula028/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula028
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 13;
+            int num2 = 2;
+
+            Console.WriteLine("Para os valores 13 e 2:");
+
+            Console.Write("Soma -> ");
+            Console.WriteLine(num1 + num2);
+
+            Console.Write("Diferenca -> ");
+            Console.WriteLine(num1 - num2);
+
+            Console.Write("Multiplicacao -> ");
+            Console.WriteLine(num1 * num2);
+
+            Console.Write("Divisao -> ");
+            Console.WriteLine(num1 / num2); // Por algum motivo, nao mostra as casa decimais...
+
+            Console.Write("Resto da divisao: ");
+            Console.WriteLine(num1 % num2);
+        }
+    }
+}
