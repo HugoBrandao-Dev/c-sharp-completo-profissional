5871b6c6493eec1737e3c7e41061717bacdf661b
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-24 20:28:27 -0300

    Add operadores de atribuicao

---
+++ secao007/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 1;
+            string nome = "Tobias";
+
+            num1 += 14;
+            Console.Write("1 + 14 = ");
+            Console.WriteLine(num1);
+
+            num1 -= 5;
+            Console.Write("15 - 5 = ");
+            Console.WriteLine(num1);
+
+            num1 *= 4;
+            Console.Write("10 * 4 = ");
+            Console.WriteLine(num1);
+
+            num1 /= 2;
+            Console.Write("40 / 2 = ");
+            Console.WriteLine(num1);
+
+            num1 %= 3;
+            Console.Write("20 % 3 = ");
+            Console.WriteLine(num1);
+
+            nome += " de Oliveira";
+
+            Console.WriteLine(num1);
+            Console.WriteLine(nome);
+        }
+    }
+}
