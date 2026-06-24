ad82e1f903b81354dc4087de961d8656489da1e3
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-24 20:53:53 -0300

    Add operadores de igualdade

---
+++ secao007/aula033/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula033
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool resultIguais = 100 == 50 * 2.1;
+            bool resultDif = 25 != 22;
+
+            string nome = "Josias";
+            bool nomesIguais = nome == "Josias";
+
+            Console.WriteLine("100 == 50 * 2.1");
+            Console.WriteLine(resultIguais);
+
+            Console.WriteLine("\n25 != 22");
+            Console.WriteLine(resultDif);
+
+            Console.WriteLine("\nJosias == Josias");
+            Console.WriteLine(nomesIguais);
+        }
+    }
+}
