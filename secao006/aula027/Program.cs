7080694707057f509c58401a535db8f5cce54a94
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-24 00:00:00 -0300

    Add classe convert

---
+++ secao006/aula027/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula027
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string num1 = "1234";
+            string num2 = "89709";
+            char meuChar = 'h';
+            bool haString = false;
+
+            int meuNum1 = Convert.ToInt32(num1);
+            Console.WriteLine(meuNum1);
+
+            long meuNum2 = Convert.ToInt64(num2);
+            Console.WriteLine(meuNum2);
+
+            sbyte meuByte = Convert.ToSByte(meuChar);
+            Console.WriteLine(meuByte);
+
+            sbyte meuByteHaString = Convert.ToSByte(haString);
+            Console.WriteLine(meuByteHaString);
+            Console.WriteLine(Convert.ToBoolean(meuByteHaString));
+        }
+    }
+}
