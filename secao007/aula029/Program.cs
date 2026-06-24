6610fd4a31ef89e8505fa0b79db67127362a1675
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-24 17:13:14 -0300

    Add precedencia operadores aritmeticos

---
+++ secao007/aula029/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula029
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1 = 100;
+            float num2 = 13;
+            float num3 = 4;
+
+            float result = (num2 / (num1 + num2 + num3)) * 100;
+
+            Console.WriteLine(result);
+        }
+    }
+}
