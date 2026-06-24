280eb77428d1c604ba8d494993bd4678b9c6d8f0
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-24 17:29:20 -0300

    Add operadores de incremento e decremento

---
+++ secao007/aula030/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula030
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 9;
+
+            // num1 = num1 + 1;
+
+            #region Operacoes com num1
+            num1++;
+            Console.WriteLine(--num1); // Antes de mostrar, ele diminui 1
+            Console.WriteLine(num1++); // Mostra antes de somar
+            #endregion
+
+            #region Operacoes com num2
+            num2--;
+            Console.WriteLine(++num2); // Antes de mostrar, ele soma 1
+            Console.WriteLine(num2--); // Mostra antes de diminuir
+            #endregion
+        }
+    }
+}
