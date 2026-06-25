32bf7882506d0ac8b428f6c85390849cc1df2e6c
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-25 10:12:20 -0300

    Add operadores relacionais

---
+++ secao007/aula034/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula034
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 45 + 60;
+            int div = 100 / 2;
+
+            // Operadores aritmeticos tem prioridade a cima dos relacionais
+            bool menor = 100 < soma;
+            Console.WriteLine("100 < 45 + 60");
+            Console.WriteLine(menor);
+
+            bool maior = 50 > 30 * 2;
+            Console.WriteLine("\n50 > 30 * 2");
+            Console.WriteLine(maior);
+
+            bool menorIgual = 41 <= 20 + 21;
+            Console.WriteLine("\n41 <= 20 + 21");
+            Console.WriteLine(menorIgual);
+
+            bool maiorIgual = 39 >= div;
+            Console.WriteLine("\n39 >= 100 / 2");
+            Console.WriteLine(maiorIgual);
+        }
+    }
+}
