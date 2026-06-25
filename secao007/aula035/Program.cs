e69e3899ff55aa35e7cf5abd4cbf1dc33d1c67bb
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-25 10:37:11 -0300

    Add operadores logicos

---
+++ secao007/aula035/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula035
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 23;
+            int b = 21;
+            int x = 67;
+            int y = 45;
+
+            Console.Write("(23 > 21) && (67 == 45): ");
+            Console.WriteLine((a > b) && (x == y));
+
+            Console.Write("(23 > 21) || (67 == 45): ");
+            Console.WriteLine((a > b) || (x == y));
+        }
+    }
+}
