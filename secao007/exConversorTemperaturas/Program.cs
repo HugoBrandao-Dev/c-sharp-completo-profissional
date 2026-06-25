35545606e7e8c0cb3c7b01c9379266a4853ce09b
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-25 11:13:45 -0300

    Add ex conversor de temperatura

---
+++ secao007/exConversorTemperaturas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exConversorTemperaturas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double cel, fahr, kel;
+
+            Console.WriteLine("### CONVERSOR DE TEMPURATURAS ###");
+
+            Console.Write("Temperatura (°C): ");
+            cel = double.Parse(Console.ReadLine());
+
+            fahr = cel * 9 / 5 + 32;
+            Console.WriteLine(cel + " °C = " + fahr + " °F");
+
+            kel = cel + 273.15;
+            Console.WriteLine(cel + " °C = " + kel + " °K");
+            Console.ReadKey();
+        }
+    }
+}
