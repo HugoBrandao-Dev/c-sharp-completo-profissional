033e63b7cea1add3aaa82f07773d044db1ebd012
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-29 18:26:39 -0300

    Add repeticao do while

---
+++ secao011/aula050/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula050
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            int valor = 0;
+
+            do
+            {
+                Console.Write("Informe um valor (0 - SAIR): ");
+                valor = int.Parse(Console.ReadLine());
+
+                result += valor;
+            }
+            while (valor != 0);
+
+            Console.WriteLine("\nA soma dos valores é " + result);
+            Console.ReadKey();
+        }
+    }
+}
