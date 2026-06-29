3f79bfa9212c70d433572b449734e309d065ef9f
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-29 20:10:41 -0300

    Add exerc fibonacci

---
+++ secao011/exFibonacci_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exFibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            short qtd;
+
+            Console.WriteLine("=> SEQUÊNCIA FIBONACCI <=");
+            Console.WriteLine("\nQuantos valor quer que eu mostre? ");
+            Console.Write("> ");
+            qtd = short.Parse(Console.ReadLine()); 
+
+            for (int i = 1, a = 0, b = 1, fib = 0; i <= qtd; i++, a = b, b = fib, fib = a + b)
+            {
+                Console.Write(fib + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ secao011/exFibonacci_2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("exFibonacci_2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("exFibonacci_2")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("092a323a-e5f5-4dac-9baf-7cef5bf7b1b0")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
