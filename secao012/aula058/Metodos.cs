deaf81e1c7376f76aa0ae86e499a2a5037ce0cb8
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-30 14:47:58 -0300

    Add metodos com parametros

---
+++ secao012/aula058/Metodos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula058
+{
+    internal class Metodos
+    {
+        // Métodos simples
+        public void Apresentacao()
+        {
+            Console.WriteLine("=> SOMADOR <=");
+        }
+        public void Cumprimentar(string nome, short ano)
+        {
+            Console.WriteLine("\nOlá, seja bem vindo " + nome + ". Esse programa está rodando em " + ano + ".\n");
+        }
+
+        // Métodos com parâmetros
+        public void Somar(int num1, int num2)
+        {
+            int soma = num1 + num2;
+            Console.WriteLine(num1 + " + " + num2 + " = " + soma);
+        }
+    }
+}
