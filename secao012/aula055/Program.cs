357ddc1d06a9cd9bda854a9a5e7874d0e9aed95f
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-30 10:47:10 -0300

    Add classes e objetos em poo

---
+++ secao012/aula055/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula055
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MinhaClasseInterna classeInterna = new MinhaClasseInterna();
+            MinhaClasseExterna classeExterna = new MinhaClasseExterna();
+        }
+    }
+
+    class MinhaClasseInterna
+    {
+
+    }
+}
