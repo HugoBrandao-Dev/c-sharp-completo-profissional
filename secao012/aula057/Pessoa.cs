676cb61fc0fbdc67574634e567c0022b7222b4fe
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-30 12:20:46 -0300

    Add metodos simples

---
+++ secao012/aula057/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula057
+{
+    internal class Pessoa
+    {
+        // Atributos / Campos
+        public string nome;
+        public string email;
+        public string cpf;
+
+        // Métodos simples
+        public void Cumprimentar()
+        {
+            Console.WriteLine("Olá, tudo bem " + nome + "?");
+        }
+    }
+}
---
+++ secao012/aula057/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula057
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            p1.nome = "Tobias de Oliveira";
+            p1.email = "tobias@gmail.com";
+            p1.cpf = "11111111111";
+            p1.Cumprimentar();
+
+            Pessoa p2 = new Pessoa();
+            p2.nome = "Josias Cruz";
+            p2.email = "josias@outlook.com";
+            p2.cpf = "22222222222";
+            p2.Cumprimentar();
+
+            Console.ReadKey();
+        }
+    }
+}
