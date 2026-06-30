c91feb6231dd982d4ee0adc41bdb3f1b1e18ce62
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-06-30 11:11:21 -0300

    Add atributos de classes

---
+++ secao012/aula056/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula056
+{
+    internal class Pessoa
+    {
+        // Atributos
+        public string nome;
+        public string cpf;
+        public string email;
+    }
+}
