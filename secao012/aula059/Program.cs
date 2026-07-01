a2231882e94d81e781b7ade634b132c2c7640173
Hugo Brandao - IFRO <hugobrandao.dev@gmail.com>
2026-06-30 20:27:01 -0400

    Add parametros por valor e referencia

---
+++ secao012/aula059/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula058
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor1 = 100;
+            int valor2 = 100;
+            Metodos meuMetodo = new Metodos();
+
+            // Parâmetro por valor.
+            meuMetodo.AumentarParametroValor(valor1);
+            Console.WriteLine(valor1);
+
+            Console.WriteLine();
+
+            // Parâmetro por referência.
+            meuMetodo.AumentarParametroReferencia(ref valor2);
+            Console.WriteLine(valor2);
+
+            Console.ReadKey();
+        }
+    }
+}
