8c17b0fa258b23420b0bc53b70fa03e32d7ce432
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-07-01 09:55:27 -0300

    Add secao012

---
+++ secao012/aula059/Metodos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula058
+{
+    internal class Metodos
+    {
+        public void AumentarParametroValor(int valor)
+        {
+            valor += 10;
+            Console.WriteLine("Por valor (+10): " + valor);
+        }
+
+        public void AumentarParametroReferencia(ref int valor)
+        {
+            valor += 5;
+            Console.WriteLine("Por referência (+5): " + valor);
+        }
+    }
+}
