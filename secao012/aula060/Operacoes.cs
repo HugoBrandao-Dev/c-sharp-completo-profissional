90c08ce6349fd873ebb1f13c51c45963e48b5ba1
Hugo Brandao - IFRO <hugobrandao.dev@gmail.com>
2026-06-30 20:59:16 -0400

    Add retorno de metodos

---
+++ secao012/aula060/Operacoes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula060
+{
+    internal class Operacoes
+    {
+        public float Somar(float a, float b)
+        {
+            return a + b;
+        }
+
+        public float Subtrair(float a, float b)
+        {
+            return a - b;
+        }
+
+        public float Multiplicar(float a, float b)
+        {
+            return a* b;
+        }
+
+        // Há um problema com essa funcao, pq pode nao consigo retornar algo q nao seja matematico.
+        public float Dividir(float a, float b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Não é possivel uma divisão por zero");
+                return '=';
+            }
+            return a / b;
+        }
+    }
+}
