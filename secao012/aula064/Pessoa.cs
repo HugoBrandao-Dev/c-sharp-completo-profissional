dd551c12f300c74dacb9769d249d564089999517
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-07-01 20:03:48 -0300

    Add metodo construtor

---
+++ secao012/aula064/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿namespace aula064;
+
+public class Pessoa
+{
+    public string nome;
+    public string sobrenome;
+    public int anoNascimento;
+    public int idade;
+
+    public Pessoa()
+    {
+        nome = "Desconhecido";
+        sobrenome = "Nenhum";
+        anoNascimento = 0;
+        idade = 0;
+    }
+}
---
+++ secao012/aula064/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace aula064;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa p1 = new Pessoa();
+        
+        Console.WriteLine(p1.nome);
+        Console.WriteLine(p1.sobrenome);
+        Console.WriteLine(p1.anoNascimento);
+        Console.WriteLine(p1.idade);
+
+        Console.ReadKey();
+    }
+}
