3e6c572afafad5d35a40b0fecc503136985097e3
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-07-02 14:59:18 -0300

    Add classes estaticas

---
+++ secao012/aula066/Matematica.cs
@@ -0,0 +1,39 @@
+﻿namespace aula066;
+
+public static class Matematica
+{
+    public static float a, b, c;
+    
+    public static string DescreverParabola()
+    {
+        string msgConcav = "";
+        string msgPosicao = "";
+        string msgCorteY = "";
+
+        // Verifica o a
+        if (a > 0)
+        {
+            msgConcav = "cima";
+        } else if (a < 0)
+        {
+            msgConcav = "baixo";
+        }
+        
+        // Verifica o b
+        if (b > 0)
+        {
+            msgPosicao = "esquerda";
+        } else if (b < 0)
+        {
+            msgPosicao = "direita";
+        }
+
+        return "Concavidade para: " + msgConcav +
+               "\nEstá à " + msgPosicao + "do eixo Y." +
+               "\nCorta o eixo Y em " + c;
+    }
+    public static float Delta()
+    {
+        return b * b - 4 * a * c;
+    }
+}
