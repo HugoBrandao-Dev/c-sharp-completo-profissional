066242db8e65070e040fc712571bf4944b3632db
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-07-02 15:17:54 -0300

    Add membros estaticos

---
+++ secao012/aula067/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace aula067;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa.maioridade = 21;
+
+        Pessoa p1 = new Pessoa();
+        p1.nome = "Tobias";
+        p1.idade = Pessoa.CalcularIdade(1990);
+        p1.Apresentar();
+        
+        Console.WriteLine(p1.nome);
+        Console.WriteLine(p1.idade);
+        Console.WriteLine(Pessoa.maioridade);
+
+        Console.ReadKey();
+    }
+}
