1a746a7b54aa4ab9b98f192a0c56138c1f5b5242
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-07-02 11:05:08 -0300

    Add sobrecarga de construtor

---
+++ secao012/aula065/Pessoa.cs
@@ -0,0 +1,41 @@
+﻿namespace aula065;
+
+public class Pessoa
+{
+    public string nome;
+    public string sobrenome;
+    public int anoNascimento;
+    public int idade;
+
+    #region Sobrecarga de Construtor
+    public Pessoa()
+    {
+        nome = "Desconhecido";
+        sobrenome = "Nenhum";
+        anoNascimento = 0;
+        idade = 0;
+    }
+
+    // Sobrecarga para a setagem de valores.
+    public Pessoa(string nome, string sobrenome, int anoNascimento)
+    {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.anoNascimento = anoNascimento;
+        this.idade = Idade();
+    }
+    
+    public Pessoa(string nome, string sobrenome)
+    {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.anoNascimento = 2008;
+        this.idade = Idade();
+    }
+    #endregion
+
+    private int Idade()
+    {
+        return 2026 - anoNascimento;
+    }
+}
---
+++ secao012/aula065/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace aula065;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa p1 = new Pessoa("Tobias", "de Oliveira", 1990);
+        Console.WriteLine("#1 Pessoa:");
+        Console.WriteLine(p1.nome);
+        Console.WriteLine(p1.sobrenome);
+        Console.WriteLine(p1.anoNascimento);
+        Console.WriteLine(p1.idade);
+
+        Pessoa p2 = new Pessoa("Doralice", "Cruz");
+        Console.WriteLine("\n#2 Pessoa:");
+        Console.WriteLine(p2.nome);
+        Console.WriteLine(p2.sobrenome);
+        Console.WriteLine(p2.anoNascimento);
+        Console.WriteLine(p2.idade);
+
+        Console.ReadKey();
+    }
+}
