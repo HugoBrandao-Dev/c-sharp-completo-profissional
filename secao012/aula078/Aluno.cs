8ee21a43470f1685b9e7c4a50b024c8e90ce35a2
Hugo Brandao <hugobrandao.dev@gmail.com>
2026-07-06 18:27:53 -0300

    Add exerc media geral

---
+++ secao012/aula078/Aluno.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula078
+{
+    internal class Aluno
+    {
+        public string Nome { get; private set; }
+
+        private float[] _notas;
+
+        public float Media
+        {
+            get { return CalcularMedia(); }
+        }
+
+        public Aluno(string nome, ushort provas)
+        {
+            Nome = nome;
+            _notas = new float[provas];
+        }
+
+        public void InserirNotas()
+        {
+            for (int i = 0; i < _notas.Length; i++)
+            {
+                Console.Write("Nota #" + (i + 1) + ": ");
+                _notas[i] = float.Parse(Console.ReadLine());
+            }
+        }
+
+        private float CalcularMedia()
+        {
+            float total = 0;
+
+            foreach (float nota in _notas)
+            {
+                total += nota;
+            }
+
+            return total / _notas.Length;
+        }
+    }
+}
